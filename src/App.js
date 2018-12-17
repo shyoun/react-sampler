01f8c3c0de7f353e621ea4e74b9f93f977d2e6bd
shyoun <27shyoun@gmail.com>
2018-12-17 21:22:53 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { UncontrolledAlert } from 'reactstrap'
 import './App.css';
 
 class App extends Component {
+  state = {
+    visible: true
+  }
+
+  onDismiss = () => this.setState({visible: false})
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <UncontrolledAlert color="info">
+          I am an alert and I can be dismissed!
+        </UncontrolledAlert>
       </div>
     );
   }
